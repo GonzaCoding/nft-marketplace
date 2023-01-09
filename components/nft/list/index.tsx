c9f146c376841ebe691c3ac6453874aa40adb010
GonzaCoding <iglesias.gh@gmail.com>
2023-01-09 08:39:59 -0300

    NftItem props

--- components/nft/list/index.tsx
+++ components/nft/list/index.tsx
@@ -12,7 +12,9 @@ const NftList: FunctionComponent<NftListProps> = ({ nfts }) => {
       {
         nfts.map(nft => (
           <div key={nft.image} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-            <NftItem />
+            <NftItem
+              item={nft}
+            />
           </div>
         ))
       }  
