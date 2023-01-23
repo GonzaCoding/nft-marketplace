6ac8b19c100ef21aac26136ab772810037cd85fe
GonzaCoding <iglesias.gh@gmail.com>
2023-01-23 13:18:28 -0300

    Connect to contract

--- components/providers/web3/utils.ts
+++ components/providers/web3/utils.ts
@@ -1,5 +1,5 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
-import { Contract, providers } from "ethers";
+import { Contract, providers, ethers } from "ethers";
 
 export type Web3Params = {
   ethereum: MetaMaskInpageProvider | null;
@@ -22,4 +22,27 @@ declare global {
   interface Window {
     ethereum: MetaMaskInpageProvider;
   }
+}
+
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
+
+export const loadContract = async (name: string, provider: providers.Web3Provider): Promise<Contract> => {
+
+  if (!NETWORK_ID) return Promise.reject("Network ID is not defined");
+
+  const response = await fetch(`/contracts/${name}.json`);
+  const Artifact = await response.json();
+
+  if (Artifact.networks[NETWORK_ID].address) {
+    const contract = new ethers.Contract(
+      Artifact.networks[NETWORK_ID].address,
+      Artifact.abi,
+      provider,
+    );
+
+    return contract;
+  } else {
+    return Promise.reject(`Contract: [${name}] cannot be loaded!`);
+  }
+
 }
