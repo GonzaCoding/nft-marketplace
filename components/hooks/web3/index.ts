a27f5643d89ac66770538e07be740dc7bcd9a8ba
GonzaCoding <iglesias.gh@gmail.com>
2023-01-23 18:20:46 -0300

    Handle account change

--- components/hooks/web3/index.ts
+++ components/hooks/web3/index.ts
@@ -2,7 +2,7 @@ import { useHooks } from "@providers/web3"
 
 export const useAccount = () => {
   const hooks = useHooks();
-  const swrResponse = hooks.useAccount("");
+  const swrResponse = hooks.useAccount();
 
   return {
     account: swrResponse
--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,18 +1,63 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
+import { useEffect } from 'react';
 
-type AccountHookFactory = CryptoHookFactory<string, string>;
+type UseAccountResponse = {
+  connect: () => void;
+} 
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>;
 
 export type UseAccountHook = ReturnType<AccountHookFactory>;
 
-export const hookFactory: AccountHookFactory = (deps) => (params) => {
+export const hookFactory: AccountHookFactory = ({provider, ethereum}) => (params) => {
   
-  const swrResponse = useSWR("web3/useAccount", () => {
-    console.log(deps);
-    console.log(params);
-    return "Test User";
+  const swrResponse = useSWR(
+    provider ? "web3/useAccount" : null,
+    async () => {
+      const accounts = await provider!.listAccounts();
+      const account = accounts[0];
+
+      if (!account) {
+        throw "Cannot retreive account! Please, connect to web3 wallet."
+      }
+
+      return account;
+    }, {
+      revalidateOnFocus: false,
+    }
+  );
+
+  useEffect(() => {
+    ethereum?.on("accountsChanged", handleAccountsChanged);
+
+    return () => {
+      ethereum?.removeListener("accountsChanged", handleAccountsChanged);
+    }
   });
 
-  return swrResponse;
+  const handleAccountsChanged = (...args: unknown[]) => {
+    const accounts = args[0] as string[];
+    if (accounts.length === 0) {
+      console.error("Please, connect to Web3 wallet.");
+    } else if (accounts[0] !== swrResponse.data) {
+      alert("accounts has changed");
+      console.log(accounts[0]);
+    }
+  }
+
+
+  const connect = async () => {
+    try {
+      ethereum?.request({ method: "eth_requestAccounts"})
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return {
+    ...swrResponse,
+    connect
+  };
 }
 
