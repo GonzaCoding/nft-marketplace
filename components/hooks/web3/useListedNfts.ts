c8c207a89c9edf46efdf2f12e3c5a160f738de32
GonzaCoding <iglesias.gh@gmail.com>
2023-01-26 14:01:29 -0300

    Display NFTs

--- components/hooks/web3/useListedNfts.ts
+++ components/hooks/web3/useListedNfts.ts
@@ -1,12 +1,13 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import { Nft } from "@_types/nft";
 import useSWR from "swr";
+import { ethers } from 'ethers';
 
 type UseListedNftsResponse = {
   
 } 
 
-type ListedNftsHookFactory = CryptoHookFactory<string, UseListedNftsResponse>;
+type ListedNftsHookFactory = CryptoHookFactory<Nft[], UseListedNftsResponse>;
 
 export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>;
 
@@ -15,9 +16,23 @@ export const hookFactory: ListedNftsHookFactory = ({contract}) => () => {
   const swrResponse = useSWR(
     contract ? "web3/useListedNfts" : null,
     async () => {
+      const nfts = [] as Nft[];
       const coreNfts = await contract!.getAllNftsOnSale();
 
-      const nfts = [] as any;
+      coreNfts.forEach(async (nft) => {
+        const tokenURI = await contract!.tokenURI(nft.tokenId);
+        const metaResponse = await fetch(tokenURI);
+        const metaData = await metaResponse.json();
+
+        nfts.push({
+          price: parseFloat(ethers.utils.formatEther(nft.price)),
+          tokenId: nft.tokenId.toNumber(),
+          creator: nft.creator,
+          isListed: nft.isListed,
+          meta: metaData,
+        })
+      });
+      
       return nfts;
     }
   );
